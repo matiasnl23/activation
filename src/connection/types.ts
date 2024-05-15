a4fcf1900bbcd1fec7aec9ebab672228f3166335
Matías <mlobo@qubeyond.com>
2024-05-15 09:28:37 -0300

    Document some functions

--- src/connection/types.ts
+++ src/connection/types.ts
@@ -13,15 +13,41 @@ export interface ConnectionOptions {
   pingOfflineMaxDelay?: number;
   pingOnlineDelay?: number;
 }
-
-export type PingFn = (server: Connection) => Promise<void>;
-export type AuthenticationFn = (server: Connection) => Promise<string>;
+export type PingFn = (client: Connection) => Promise<void>;
+export type AuthenticationFn = (client: Connection) => Promise<string>;
 
 export interface ConnectionManagerOptions {
-  onConnectionStatus?: (server: string, status: boolean) => void;
-  onAuthenticated?: (server: string) => void;
-  onUnauthenticated?: (server: string) => void;
-  onMaxAttemptsReached?: (server: string) => void;
+  onConnectionStatus?: (client: string, status: boolean) => void;
+  onAuthenticated?: (client: string) => void;
+  onUnauthenticated?: (client: string) => void;
+  onMaxAttemptsReached?: (client: string) => void;
+
+  /**
+    * Function used for authenticating, this function must
+    * return the token or throw an error if the authentication fails.
+    *
+    * @example
+    * async (client) => {
+    *   const result = await fetch(`${client.baseUrl}/auth`)
+    *     .then(r => r.json());
+    *
+    *   if (!result.token) throw new Error("Invalid credentials.");
+    *
+    *   return result.token;
+    * }
+    */
   authenticationFn: AuthenticationFn;
+
+  /**
+    * Function used to identify if the backend is online
+    *
+    * @example
+    * async (client) => {
+    *   const result = await fetch(`${client.baseUrl}/status`)
+    *     .then(r => r.json());
+    *
+    *   if (!result.status) throw new Error("Server is offline.");
+    * }
+    */
   pingFn: PingFn;
 }
